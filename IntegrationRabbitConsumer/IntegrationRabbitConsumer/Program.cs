b1b54d55c2a545cdf0427fcd7dabcfdebc8d7ccc
rodrigo33 <rbarbosa.barbosa@gmail.com>
2023-03-27 23:05:40 -0300

    consumo, criação da mensagem utilizando RabbitMQ

---
+++ IntegrationRabbitConsumer/IntegrationRabbitConsumer/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace IntegrationRabbitConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "saudacao_1",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] Recebida : {message}");
+                };
+
+                channel.BasicConsume(queue: "saudacao_1",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ProjectIntegrationRabbit/ProjectIntegrationRabbit/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace ProjectIntegrationRabbit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "saudacao_1",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                string message = "RabbitMQ";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                    routingKey: "saudacao_1",
+                    basicProperties: null,
+                    body: body);
+
+                Console.WriteLine($" [x] Enviada : {message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
